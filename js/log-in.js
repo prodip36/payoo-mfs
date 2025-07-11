4820298b65b2fae6139b7121495596fde3dbe38a
nothing36 <prodipsarker953@gmail.com>
2025-07-11 21:37:20 +0600

    js pass and pin matching

--- js/log-in.js
+++ js/log-in.js
@@ -2,11 +2,20 @@
 document.getElementById('log-in').addEventListener('click',function(event){
     
     event.preventDefault();
-    console.log('log in button clicked');
+    // console.log('log in button clicked');
     //Phone number
     const phoneNumber=document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+    // console.log(phoneNumber.value);
     //Pin number
     const pinNumber=document.getElementById('pin-number');
-    console.log(pinNumber.value);
+    // console.log(pinNumber.value);
+    if(phoneNumber.value === "01860931442" && pinNumber.value === "1234")
+    {
+        console.log("You are logged in.")
+        window.location.href='./home.html';
+    }
+    else{
+        alert('Wrong phone or pin number');
+    }
+
 })
